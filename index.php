811e2836a50f42f58e0b4c824fe992df8cec1706
Aniket Dhumal <anikethd1410@gmail.com>
2021-03-10 16:17:05 +0530

    changes some

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-index</title>
+</head>
+<body>
+<a href="ifstatement.php">IF Statement </a>
+    <h1>Hello World php index</h1>
+    <?php 
+         echo "Hello PHP";
+         echo "<br/>";
+         echo "Hello2";
+    ?>
 
-echo "Hello World";
-echo "this is first application while i was learning PHP"
-
-?>
+    <?php
+    $name = 'Aniket Dhumal';
+    $mobno = '7620282042';
+    echo '<h1> MY NAME IS '.$name.$mobno.'</h1>';
+    ?>
+</body>
+</html>
