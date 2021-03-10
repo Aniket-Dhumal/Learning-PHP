70b4ed774a6ea657ea195394dd72ec46a87fca85
Aniket Dhumal <anikethd1410@gmail.com>
2021-03-10 16:14:08 +0530

    some changes & test passed

---
+++ ifstatement.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF Statement</title>
+</head>
+<body>
+    <?php
+    date_default_timezone_set("Asia/Calcutta");
+    
+    $today = date("dmhi");
+    while(1){
+   echo $today;
+    echo "<h2>Date & Time</h2>";
+    $Datetime = 1003;
+
+    if($Datetime == $today){
+        echo '<h3>YOU PASSED</h3>';
+    }
+    else{
+        echo 'You failed';
+    }
+}
+
+    ?>
+</body>
+</html>
